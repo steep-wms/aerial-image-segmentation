d06c110601940eb25c9e5e1654a7542e5cd84baa
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2023-08-24 15:10:48 +0200

    Add segment service

---
+++ segment-service/segment.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import sys
+import matplotlib.pyplot as plt
+from samgeo import SamGeo
+
+if len(sys.argv) != 3:
+    print("Usage: python3 segment.py <input_image> <output_image>")
+    exit(1)
+
+sam = SamGeo(checkpoint='sam_vit_h_4b8939.pth')
+sam.generate(sys.argv[1])
+sam.save_masks()
+
+plt.imsave(sys.argv[2], sam.objects, cmap='Blues')
