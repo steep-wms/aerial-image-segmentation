b0da843f7c69752512d8729917be86bb3c1fd12a
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2023-08-25 08:53:52 +0200

    Revert "Set correct model type"
    This reverts commit 655eca6ad8cf155d466ca42af490894722be7166.

--- segment-service/segment.py
+++ segment-service/segment.py
@@ -8,7 +8,7 @@ if len(sys.argv) != 3:
     print("Usage: python3 segment.py <input_image> <output_image>")
     exit(1)
 
-sam = SamGeo(checkpoint='sam_vit_b_01ec64.pth', model_type='vit_b')
+sam = SamGeo(checkpoint='sam_vit_b_01ec64.pth')
 sam.generate(sys.argv[1])
 sam.save_masks()
 
